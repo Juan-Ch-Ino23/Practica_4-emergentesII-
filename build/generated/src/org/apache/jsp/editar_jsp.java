e7c2a66727839193c0f509141683566fc41d42b2
Juan-Ch-Ino23 <52325787+Juan-Ch-Ino23@users.noreply.github.com>
2020-06-09 16:53:55 -0500

    Add files via upload

---
+++ build/generated/src/org/apache/jsp/editar_jsp.java
@@ -0,0 +1,110 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import com.emergentes.modelo.Libro;
+
+public final class editar_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+
+    Libro libro=(Libro)request.getAttribute("libro");
+
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("      <h1>\n");
+      out.write("            <c:if test=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${libro.id ==0}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\">Nuevo</c:if>\n");
+      out.write("             <c:if test=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${libro.id !=0}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\">/Editar Entrada</c:if>\n");
+      out.write("        </h1>\n");
+      out.write("        <form action=\"MainController\" method=\"post\">\n");
+      out.write("            <table border=\"1\">\n");
+      out.write("                <input type=\"hidden\" name=\"id\" value=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${libro.id}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\">\n");
+      out.write("                <tr>\n");
+      out.write("                    <td>FECHA</td>\n");
+      out.write("                    <td><input type=\"text\" name=\"isbn\" value=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${libro.fecha}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\"></td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td>TITULO</td>\n");
+      out.write("                    <td><input type=\"text\" name=\"isbn\" value=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${libro.titulo}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\"></td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td>CONTENIDO</td>\n");
+      out.write("                    <td><input type=\"text\" name=\"isbn\" value=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${libro.contenido}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\"></td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td></td>\n");
+      out.write("                    <td><input type=\"submit\" value=\"enviar\"></td>\n");
+      out.write("                </tr>\n");
+      out.write("            </table>\n");
+      out.write("        </form>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
